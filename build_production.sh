f386eee95cd3952c41980d8544571d76de6d846e
RIC217 <melericcardo9400@gmail.com>
2023-03-21 21:29:09 +0100

    Updated build_production.sh script

--- build_production.sh
+++ build_production.sh
@@ -1,4 +1,5 @@
-v -prod -skip-unused -cflags -Os . -o RealTimeWebChatInV_linux_amd64 & v -prod -skip-unused -cflags -Os -os windows . -o RealTimeWebChatInV_windows_amd64.exe
-du RealTimeWebChatInV*
-strip -s RealTimeWebChatInV*
-du RealTimeWebChatInV*
+rm -r bin && mkdir bin
+v -prod -skip-unused -cflags -Os . -o bin/RealTimeWebChatInV_linux_amd64 & v -prod -skip-unused -cflags -Os -os windows . -o bin/RealTimeWebChatInV_windows_amd64.exe
+du bin/RealTimeWebChatInV_*
+strip -s bin/RealTimeWebChatInV_*
+du bin/RealTimeWebChatInV_*
