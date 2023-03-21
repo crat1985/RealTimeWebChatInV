0d837088cfdad7a4a8a3dc9eb74ca9a7e65d633d
RIC217 <melericcardo9400@gmail.com>
2023-03-21 21:30:07 +0100

    Patched build_production.sh script

--- build_production.sh
+++ build_production.sh
@@ -1,4 +1,4 @@
-rm -r bin && mkdir bin
+mkdir bin && rm -r bin && mkdir bin
 v -prod -skip-unused -cflags -Os . -o bin/RealTimeWebChatInV_linux_amd64 & v -prod -skip-unused -cflags -Os -os windows . -o bin/RealTimeWebChatInV_windows_amd64.exe
 du bin/RealTimeWebChatInV_*
 strip -s bin/RealTimeWebChatInV_*
