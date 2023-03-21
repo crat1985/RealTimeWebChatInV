a36a609720c336572600e36effc62f64ff766b9d
RIC217 <melericcardo9400@gmail.com>
2023-03-21 21:08:26 +0100

    Updated build_production.sh script

--- build_production.sh
+++ build_production.sh
@@ -1,4 +1,4 @@
-v -prod -skip-unused -cflags -Os . & v -prod -skip-unused -cflags -Os -os windows .
+v -prod -skip-unused -cflags -Os . -o RealTimeWebChatInV_linux_amd64 & v -prod -skip-unused -cflags -Os -os windows . -o RealTimeWebChatInV_windows_amd64.exe
 du RealTimeWebChatInV*
 strip -s RealTimeWebChatInV*
 du RealTimeWebChatInV*
